a8ae2a95b1f6325e6a6015938329e8d45ee1dc4f
Ksusha <o.derechinskaya@gmail.com>
2023-05-17 11:22:29 +0300

    ДЗ номер 1.2 выполнила.

---
+++ src/homework1/HomeWorkSTwo.java
@@ -0,0 +1,23 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class HomeWorkSTwo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите длину стороны треугольника:");
+        int a = in.nextInt();
+        System.out.println("Введите длину стороны треугольника:");
+        int b = in.nextInt();
+        System.out.println("Введите длину стороны треугольника:");
+        int c = in.nextInt();
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)){
+            System.out.println("Из этих сторон можно построить треугольник");
+        } else {
+            System.out.println("Из этих сторон нельзя построить треугольник");
+        }
+
+    }
+}
+
