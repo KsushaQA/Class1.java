e6eb181e67e8bbab806c63ce2b1865ba96e26f8c
Ksusha <o.derechinskaya@gmail.com>
2023-05-21 01:49:59 +0300

    Выполнила ДЗ 2.6.

--- src/homework2/HW6.java
+++ src/homework2/HW6.java
@@ -4,4 +4,25 @@ package homework2;
         на консоль. Найти максимальный элемент и вывести его на
         консоль.*/
 public class HW6 {
+    public static void main(String[] args) {
+        int[] array = new int[45];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) ((Math.random() * 101) - 50);
+            System.out.print(array[i] + " ");
+        }
+        int minValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        System.out.println("\n" + "Minimum value is: " + minValue);
+        int maxValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println("\n" + "Maximum value is: " + maxValue);
+    }
 }
