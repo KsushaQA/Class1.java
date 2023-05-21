ee2a72da5a8b4bbf0ace977c8e0266f19ab5d64e
Ksusha <o.derechinskaya@gmail.com>
2023-05-21 10:39:07 +0300

    Внесла изменения в ДЗ 2.5

--- src/homework2/HW5.java
+++ src/homework2/HW5.java
@@ -1,7 +1,34 @@
 package homework2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*5)Есть одномерный массив из 10 элементов, заполнен-
         ный случайными числами. Пользователь вводит с клавиатуры
         число. Программа показывает есть ли такое число в созданном
         до этого массиве.*/
 public class HW5 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random()*10);
+        }
+        System.out.println("Массив: "+ Arrays.toString(array));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число для поиска в массиве:");
+        int number = scanner.nextInt();
+        boolean found = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Число " + number + " найдено в массиве.");
+        } else {
+            System.out.println("Число " + number + " не найдено в массиве.");
+        }
+
+    }
 }
