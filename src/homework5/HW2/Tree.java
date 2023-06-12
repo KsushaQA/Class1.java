90e907754f99e7bbb533813a41c675056ac48d09
Ksusha <o.derechinskaya@gmail.com>
2023-06-12 23:20:47 +0300

    ДЗ номер 5.2 LMS - начало.

---
+++ src/homework5/HW2/Tree.java
@@ -0,0 +1,33 @@
+package homework5.HW2;
+/*2) Создать класс Tree с перегруженными конструкторами.
+        В классе есть Tree есть поля:
+        String type; int height, int coutOfsticks, String colour;
+
+        Конструктор1 принимающий в параметры String type; int height
+        и присваивающий значение полям coutOfsticks значение 13,
+        colour "Зеленый".
+
+        Конструктор2 принимающий в параметры int height, int coutOfsticks,
+        String colour и присваивающий значение полю type "пихта".
+
+        Констуктор по умолчанию 3, который присваивает height = 350, coutOfsticks = 29,
+        сolour = "Желтый".
+
+        Конструктор4 который принимает в параметры String type,
+        и вызывает внутри себя конструктор 3.
+
+        Создать в отдельном классе main 4 объекта класса Tree используя 4 созданных конструктора.*/
+
+public class Tree {
+    String type;
+    int height;
+    int coutOfsticks;
+    String colour;
+
+    public Tree(String type, int height, int coutOfsticks, String colour) {
+        this.type = type;
+        this.height = height;
+        this.coutOfsticks = 13;
+        this.colour = "Зеленый";
+    }
+}
