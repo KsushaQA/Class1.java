a602589144439bca392f7ad4937194f6a7084bb7
Ksusha <o.derechinskaya@gmail.com>
2023-06-18 00:06:06 +0300

    ДЗ номер 6 - класс Family начало.

--- src/homework6/Family.java
+++ src/homework6/Family.java
@@ -1,4 +1,72 @@
 package homework6;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Family {
+        private Human mother;
+        private Human father;
+        private Human[] children;
+        private Pet pet;
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Family(Human mother, Human father) {
+        this.mother = mother;
+        this.father = father;
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "mother=" + mother +
+                ", father=" + father +
+                ", children=" + Arrays.toString(children) +
+                ", pet=" + pet +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(mother, family.mother) && Objects.equals(father, family.father);
+    }
+
+    @Override
+    public int hashCode() {
+       return Objects.hash(mother, father);
+
+    }
 }
