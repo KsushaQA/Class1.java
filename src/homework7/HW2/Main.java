3f88ec4ce68af294266a792f68aa0815375a942b
Ksusha <o.derechinskaya@gmail.com>
2023-06-24 13:58:21 +0300

    ДЗ номер 7 - выполнила задачу 2.

--- src/homework7/HW2/Main.java
+++ src/homework7/HW2/Main.java
@@ -3,7 +3,6 @@ package homework7.HW2;
 public class Main {
     public static void main(String[] args) {
         ArrayDivision arrayDivision = new ArrayDivision();
-
         try {
             double result = arrayDivision.calculateDivision();
             System.out.println("Результат деления: " + result);
